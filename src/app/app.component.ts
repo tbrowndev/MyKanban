49e8eaf5824a207e4293ab44e3d827871ce03cbf
Tommy Brown <46692709+tbrowndev@users.noreply.github.com>
2022-10-18 21:04:20 -0500

    added board and drag drop functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyKanban';
+
+  todo = [
+    'Wake Up',
+    'Brush Teeth',
+    'Get Dressed',
+    'Tie Shoes',
+    'Go to Work',
+    'Complete Work Stuff',
+    'Go Home',
+    'Get ready for bed',
+    'Go to bed'
+  ];
+
+  inProgress = [];
+  onHold = [];
+  done = [];
+
+  handleColumnClick(event: any) {
+    if (event.target !== event.currentTarget) return;
+    console.log('New ToDo for board')
+  }
+
+  handleDrop(event: any) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
